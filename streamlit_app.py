6dcb8f71122d0b9301ea6761533daebac952a626
Dongsoo Lee <110653822+ds72gh@users.noreply.github.com>
2022-08-08 16:39:19 +0900

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New Sesction to display frutyvice api response
-import requests
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+#import requests
+#fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response)

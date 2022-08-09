0da3b496a166ad2d7dd7a6f3644e33bf774c4297
Dongsoo Lee <110653822+ds72gh@users.noreply.github.com>
2022-08-09 17:06:09 +0900

    Update streamlit_app.py
    added question

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# New Sesction to display frutyvice api response
+streamlit.header('What fruit would you like to add?')
+fruit_choice = streamlit.text_input('Whar fruit would you like information about?', 'Jackfruit')
+streamlit.write('Thanks for adding', fruit_choice)
